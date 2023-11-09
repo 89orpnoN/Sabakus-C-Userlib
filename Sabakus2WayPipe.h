111f07e4fb242bf92250d703b4ac6a493b6f4176
89orpnon <9698022@itisrossi.vi.it>
2023-11-09 23:35:43 +0100

    Added Sabakus2WayPipes

--- Sabakus2WayPipe.h
+++ Sabakus2WayPipe.h
@@ -1,8 +1,46 @@
-//
-// Created by Nonpro98 on 09/11/2023.
-//
-
 #ifndef SABAKUS_C_USERLIB_SABAKUS2WAYPIPE_H
 #define SABAKUS_C_USERLIB_SABAKUS2WAYPIPE_H
+
+#include <unistd.h>
+#include "SabakusVoidArr.h"
+
+enum Types {
+    Void, //se lo usi sei malato
+    String,
+    Int,
+    Float,
+};
+int TypeLenghts[4] = {
+        1,
+        sizeof(string),
+        sizeof(int),
+        sizeof(float)
+};
+
+typedef struct {
+    int UpStream[2];
+    int DownStream[2];
+    short short int Type;
+    short short int ChunkSize;
+    int Pid1;
+    int Pid2;
+
+}S2WPipe;
+
+typedef struct {
+void* Data;
+short int Type;
+}SPReadings;
+
+S2WPipe S2WPipeConstructor(int Pid1,int Pid2,int type){
+    S2WPipe Pipe;
+    if (pipe(Pipe.UpStream)<0 || pipe(Pipe.DownStream)<0) return Pipe;
+    Pipe.Pid1 = Pid1;
+    Pipe.Pid2 = Pid2;
+    Pipe.Type = type;
+    Pipe.ChunkSize = TypeLenghts[type]
+}
+
+void*
 
 #endif //SABAKUS_C_USERLIB_SABAKUS2WAYPIPE_H
